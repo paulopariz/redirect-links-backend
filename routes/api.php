878261a5e458da9f8357a3b8917f09df4f187a86
paulopariz <paulopariz01@gmail.com>
2023-01-09 19:40:41 -0300

    routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\listController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/lista', [listController::class, 'listagem']);
+Route::get('/adicionar', [listController::class, 'adicionar']);
+Route::get('/editar/{link}', [listController::class, 'editar']);
+Route::get('/atualizar/{link}', [listController::class, 'atualizar']);
+Route::get('/deletar/{link}', [listController::class, 'deletar']);
