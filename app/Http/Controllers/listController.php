a4dfe61c71ebae988b92444aa85e3478d30e021e
paulopariz <paulopariz01@gmail.com>
2023-01-09 20:04:46 -0300

    actions list controller

--- app/Http/Controllers/listController.php
+++ app/Http/Controllers/listController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ListLink;
 use Illuminate\Http\Request;
 
 class listController extends Controller
 {
-    //
+    public function listagem(Request $request)
+    {
+        $ListLink = ListLink::all();
+
+        return response()->json($ListLink);
+    }
+
+    public function adicionar(Request $request)
+    {
+        $newLink = new ListLink();
+        $newLink->name = $request->name;
+        $newLink->email = $request->email;
+        $newLink->save();
+
+        return response()->json('success');
+    }
+
+    public function editar(ListLink $link, Request $request)
+    {
+        return response()->json($link);
+    }
+
+    public function atualizar(ListLink $link, Request $request)
+    {
+        $link->name = $request->name;
+        $link->email = $request->email;
+        $link->save();
+
+        return response()->json('success');
+    }
+
+    public function deletar(ListLink $link, Request $request)
+    {
+        $link->delete();
+
+        return response()->json($link);
+    }
+
+
+
+
+
+
 }
